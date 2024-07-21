a30580a3521ad31c2a026cfecdd139d2cc2d58c1
acarley33 <acarley@live.com.mx>
2024-07-21 08:23:51 -0600

    Add files via upload

---
+++ testyuploadfile.py
@@ -0,0 +1,9 @@
+Soundcore By Anker Audífonos Life Q30, Cancelación De Ruido
+
+https://www.amazon.com.mx/Skullcandy-Auriculares-inal%C3%A1mbricos-compatibilidad-tecnolog%C3%ADa/dp/B0B75MQJQR/ref=sr_1_22?crid=2N57ZC9YACPD9&dib=eyJ2IjoiMSJ9.IoZfpPdxakfUEIq3ZfjynH4CDPgU26s_H7R-kiCvCqY6RiAYEUKxraX07OIuyb8srhvtiSeL-Rr6DJ2twXdX6mN5dm_dg5ZAqFUI_Qi0JhJplA5qzLHWgTIMJFe9ueZFWxRmeVJUCpE5Aw0POlIE57ENI9PTMfwkfM5-6NM2mm0dalEf-88bNqYzRIxkOzPB8fKbVTSumeTBtfsaIFVTmb9lRz35Pe97PNNWYqwjqvq1MUL1-8KaqIEWJryn7xq5cwxCPJqYSgw9u7IjG2ul723asGW2dLNYD88PgbYMKRs.xgZoacuN-enPY-srtmLvn6EM7OcJKnIz3LaNRfuZbo8&dib_tag=se&keywords=skullcandy&qid=1721272775&s=electronics&sprefix=sku%2Celectronics%2C226&sr=1-22&ufe=app_do%3Aamzn1.fos.242f5c11-6cfd-40d6-91f6-be3d1974080c&th=1
+
+
+
+
+https://www.amazon.com.mx/Skullcandy-Auriculares-inal%C3%A1mbricos-compatibilidad-tecnolog%C3%ADa/dp/B0B75MQJQR/ref=sr_1_22?crid=2N57ZC9YACPD9&dib=eyJ2IjoiMSJ9.IoZfpPdxakfUEIq3ZfjynH4CDPgU26s_H7R-kiCvCqY6RiAYEUKxraX07OIuyb8srhvtiSeL-Rr6DJ2twXdX6mN5dm_dg5ZAqFUI_Qi0JhJplA5qzLHWgTIMJFe9ueZFWxRmeVJUCpE5Aw0POlIE57ENI9PTMfwkfM5-6NM2mm0dalEf-88bNqYzRIxkOzPB8fKbVTSumeTBtfsaIFVTmb9lRz35Pe97PNNWYqwjqvq1MUL1-8KaqIEWJryn7xq5cwxCPJqYSgw9u7IjG2ul723asGW2dLNYD88PgbYMKRs.xgZoacuN-enPY-srtmLvn6EM7OcJKnIz3LaNRfuZbo8&dib_tag=se&keywords=skullcandy&qid=1721272775&s=electronics&sprefix=sku%2Celectronics%2C226&sr=1-22&ufe=app_do%3Aamzn1.fos.242f5c11-6cfd-40d6-91f6-be3d1974080c&th=1
+
